d21ba2023aec2ee0b9859e2c8f52b48a0b4d55e3
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2022-11-09 16:00:53 -0300

    Criação da função createDirectories para criar os diretórios com as permissões necessárias

--- script.sh
+++ script.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+createDirectories(){
+    # Criando diretórios aplicando permissões indicadas no projeto
+    echo Criando diretórios com suas permissões
+    mkdir -m 777 /publico
+    mkdir -m 770 /adm
+    mkdir -m 770 /ven
+    mkdir -m 770 /sec
+}
+
+createGroups(){
+
+}
+
+createUsers(){
+
+}
